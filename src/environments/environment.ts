3e2d9cb28a59bcabd382533b8af57f3fd8f76857
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-06-03 16:53:15 -0500

    Add environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,13 +6,13 @@ export const environment = {
   production: false,
   url_api: 'https://store.nicobytes.site/api',
   firebase: {
-    apiKey: 'AIzaSyCN599W6_flBEpe0xrcuBlGPRW3OKzDr7o',
-    authDomain: 'platzi-store-forms.firebaseapp.com',
-    databaseURL: 'https://platzi-store-forms.firebaseio.com',
-    projectId: 'platzi-store-forms',
-    storageBucket: 'platzi-store-forms.appspot.com',
-    messagingSenderId: '783669493891',
-    appId: '1:783669493891:web:f973c1ba9bb213e2c75bb6'
+    apiKey: "AIzaSyCcXhJamNdzm7K-8k0vIe2rCF1w6kRrtus",
+    authDomain: "platzi-angular-4adb9.firebaseapp.com",
+    databaseURL: "https://platzi-angular-4adb9-default-rtdb.firebaseio.com",
+    projectId: "platzi-angular-4adb9",
+    storageBucket: "platzi-angular-4adb9.appspot.com",
+    messagingSenderId: "960237864193",
+    appId: "1:960237864193:web:b8e23e0e1ba2c390f48700"
   }
 };
 
