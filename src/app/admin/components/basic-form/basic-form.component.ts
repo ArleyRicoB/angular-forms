3b9538986c721b2ac7684c82ffa2045a01c4c696
Arley Sneyder Rico Bohórquez <arleyrc09@gmail.com>
2021-06-04 14:40:21 -0500

    Add some form validations

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,7 +8,8 @@ import { FormControl } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  nameField = new FormControl('I\'m a control');
+  // new FormControl (value, sync validations, async validations)
+  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
   emailField = new FormControl('');
   phoneField = new FormControl('');
   colorField = new FormControl('');
@@ -35,4 +36,11 @@ export class BasicFormComponent implements OnInit {
     console.log(this.nameField.value);
   }
 
+  get isNameFieldValid() {
+    return this.nameField.touched && this.nameField.valid;
+  }
+
+  get isNameFieldInvalid() {
+    return this.nameField.touched && this.nameField.valid;
+  }
 }
